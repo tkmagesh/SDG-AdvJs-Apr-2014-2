3797f21725b11449fe4598df683619799d5849b4
tkmagesh <tkmagesh77@gmail.com>
2014-04-28 15:52:31 +0530

    Calculator specs - assignment

--- Day-1/Langauge/calculatorSpecs.js
+++ Day-1/Langauge/calculatorSpecs.js
@@ -37,4 +37,66 @@ $(function(){
 		//assert
 		return expectedResult === result;
 	})
+
+	test("Should be able to add functions returning numbers",function(){
+		//arrange
+		var f1 = function(){ return 10;},
+			f2 = function(){ return 20;},
+			expectedResult = 30;
+		
+
+		//act
+		var result = add(f1,f2);
+		console.log(result);
+		//assert
+		return expectedResult === result;
+	})
+	test("Should be able to add functions returning numbers in string format",function(){
+		//arrange
+		var f1 = function(){ return "10";},
+			f2 = function(){ return "20";},
+			expectedResult = 30;
+		
+
+		//act
+		var result = add(f1,f2);
+		console.log(result);
+		//assert
+		return expectedResult === result;
+	})
+	test("Should be able to add functions returning functions returning numbers in string format",function(){
+		//arrange
+		var f1 = function(){ return function(){ return "10";}},
+			f2 = function(){ return function(){ return "20";}},
+			expectedResult = 30;
+		
+
+		//act
+		var result = add(f1,f2);
+		console.log(result);
+		//assert
+		return expectedResult === result;
+	})
+	test("Should return zero when no arguments are passed",function(){
+		//arrange
+		var expectedResult = 0;
+		
+
+		//act
+		var result = add();
+		console.log(result);
+		//assert
+		return expectedResult === result;
+	})
+	test("Should be able to add more than 2 numbers",function(){
+		//arrange
+		var expectedResult = 100;
+		
+
+		//act
+		var result = add(10,20,30,40);
+		console.log(result);
+		//assert
+		return expectedResult === result;
+	})
 })
